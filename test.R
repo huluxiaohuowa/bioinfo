1e47ef9b46a88cecb1666a30087f32e099e28939
huluxiaohuowa <jianxing_hu@icloud.com>
2022-11-16 19:53:18 +0800

    fix: 🐛 lalal

--- test.R
+++ test.R
@@ -1,18 +1,23 @@
 exp <- matrix(rnorm(300), nrow = 30, ncol = 10)
 
 exp[1:15, 1:5] <- exp[
-    1:15, 1:5
-] + matrix(
-    rnorm(75, mean = 4), nrow = 15, ncol = 5
-)
-exp[16:30, 6:10] <- exp[16:30, 6:10] + matrix(rnorm(75,mean = 3), nrow = 15, ncol = 5)
+        1:15, 1:5
+    ] + matrix(
+        rnorm(75, mean = 4),
+        nrow = 15, ncol = 5
+    )
+
+exp[16:30, 6:10] <- exp[
+    16:30, 6:10
+] + matrix(rnorm(75, mean = 3), nrow = 15, ncol = 5)
 
 exp <- round(exp, 2)
 colnames(exp) <- paste("Sample", 1:10, sep = "")
 rownames(exp) <- paste("Gene", 1:30, sep = "")
 head(exp)
 
-plot(nn)
+View(exp)
+
 library(pheatmap)
 ph <- pheatmap(exp)
 ph$tree_row$order
@@ -23,5 +28,4 @@ ph_exp <- exp[
     ph$tree_row$order, ph$tree_col$order
 ]
 
-ph_exp[1:4, 1:4] 
-
+ph_exp[1:4, 1:4]
