c3f2695e44f9db68bb5856e21091e405e2e5e460
jianxing.hu <jianxing.hu@xtalpi.com>
2022-11-16 17:24:34 +0800

    fix: 🐛 lala

---
+++ geo.R
@@ -0,0 +1,24 @@
+library(GEOquery)
+my_id <- "GSE17891"
+gse_la <- getGEO(my_id)
+
+
+length(gse_la)
+gse <- gse_la[[2]]
+
+pData(gse) ## print the sample information
+fData(gse) ## print the gene annotation
+exprs(gse) ## print the expression data
+
+summary(exprs(gse))
+
+exprs(gse) <- log2(exprs(gse))
+boxplot(exprs(gse),outline=FALSE)
+
+library(dplyr)
+
+sampleInfo <- pData(gse)
+View(sampleInfo)
+
+library(ggplot2)
+library(readr)
