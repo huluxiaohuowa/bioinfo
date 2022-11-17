4cf533831e9e905529829a68cb7929d8144d28a4
jianxing.hu <jianxing.hu@xtalpi.com>
2022-11-17 10:09:21 +0800

    fix: 🐛 lala

--- geo.R
+++ geo.R
@@ -17,8 +17,34 @@ boxplot(exprs(gse), outline=FALSE)
 
 library(dplyr)
 
-sampleInfo <- pData(gse)
-View(sampleInfo)
+sample_info <- pData(gse)
+
+sample_info <- dplyr::select(
+    sampleInfo,
+    source_name_ch1,
+    characteristics_ch1
+)
+
+sample_info <- dplyr::rename(
+    sample_info,
+    group = source_name_ch1,
+    patient = characteristics_ch1
+)
+
+View(sample_info)
 
 library(ggplot2)
 library(readr)
+
+
+library(ggrepel)
+
+library(pheatmap)
+## argument use="c" stops an error if there are any missing data points
+
+cor_matrix <- cor(
+    exprs(gse),
+    use = "c"
+)
+
+pheatmap(cor_matrix) 
