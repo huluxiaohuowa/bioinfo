0296c5223528ac928f3f096550a018a776a03527
jianxing.hu <jianxing.hu@xtalpi.com>
2022-11-18 14:27:35 +0800

    fix: 🐛 fix whitespace

--- percent_bar.R
+++ percent_bar.R
@@ -1,6 +1,34 @@
 df1 <- read.table(
-    'data/percent_B.tsv',
+    "data/percent_B.tsv",
     header = TRUE,
-    sep = '	',
+    sep = "\t",
     stringsAsFactors = FALSE
 )
+
+df2 <- read.table(
+    "data/percent_p.tsv",
+    header = TRUE,
+    sep = "\t",
+    stringsAsFactors = FALSE
+)
+
+library("ggplot2")
+p <- ggplot(df1, aes(x=Group, y=Percent, fill=Expresion_level)) +
+    geom_bar(stat="identity", colour="black") +
+    guides(fill=guide_legend(reverse=TRUE)) +
+    scale_fill_brewer(palette="Pastel1") +
+    theme(
+        text = element_text(size = 40),
+        axis.text = element_text(size = 33)
+    )
+ggsave(p, filename = "plot_B.png")
+
+p <- ggplot(df2, aes(x=Group, y=Percent, fill=Expresion_level)) +
+    geom_bar(stat="identity", colour="black") +
+    guides(fill=guide_legend(reverse=TRUE)) +
+    scale_fill_brewer(palette="Pastel1") +
+    theme(
+        text = element_text(size = 40),
+        axis.text = element_text(size = 33)
+    )
+ggsave(p, filename = "plot_p.png")
