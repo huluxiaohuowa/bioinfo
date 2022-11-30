1ad160a48c61910b7939eb7e933482e115f9f049
jianxing.hu <jianxing.hu@xtalpi.com>
2022-11-30 09:48:08 +0800

    feat: 🎸 GSE211669 data

--- percent_bar.R
+++ percent_bar.R
@@ -13,20 +13,30 @@ df2 <- read.table(
 )
 
 library("ggplot2")
-p <- ggplot(df1, aes(x=Group, y=Percent, fill=Expresion_level)) +
-    geom_bar(stat="identity", colour="black") +
-    guides(fill=guide_legend(reverse=TRUE)) +
-    scale_fill_brewer(palette="Pastel1") +
+p <- ggplot(
+        df1,
+        aes(x = Group, y = Percent, fill = Expresion_level)
+    ) +
+    geom_bar(stat = "identity", colour = "black") +
+    guides(fill = guide_legend(reverse = TRUE)) +
+    scale_fill_brewer(palette = "Pastel1") +
     theme(
         text = element_text(size = 40),
         axis.text = element_text(size = 33)
     )
 ggsave(p, filename = "plot_B.png")
 
-p <- ggplot(df2, aes(x=Group, y=Percent, fill=Expresion_level)) +
-    geom_bar(stat="identity", colour="black") +
-    guides(fill=guide_legend(reverse=TRUE)) +
-    scale_fill_brewer(palette="Pastel1") +
+p <- ggplot(
+    df2,
+    aes(
+        x = Group,
+        y = Percent,
+        fill = Expresion_level
+    )
+) +
+    geom_bar(stat = "identity", colour = "black") +
+    guides(fill = guide_legend(reverse = TRUE)) +
+    scale_fill_brewer(palette = "Pastel1") +
     theme(
         text = element_text(size = 40),
         axis.text = element_text(size = 33)
