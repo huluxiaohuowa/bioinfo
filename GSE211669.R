b13d3b42947f6b550d2f4cce0635453cc7699054
jianxing.hu <jianxing.hu@xtalpi.com>
2022-12-13 10:04:37 +0800

    feat: 🎸 extract BCL2A1 from raw data

---
+++ GSE211669.R
@@ -0,0 +1,183 @@
+library(GEOquery)
+my_id <- "GSE211669"
+gse_la <- getGEO(my_id)
+
+length(gse_la)
+
+gse <- gse_la[[2]]
+sam <- pData(gse) ## the sample information
+anno <- fData(gse) ## the gene annotation
+ex <-  exprs(gse) ## the expression data
+View(ex)
+summary(exprs(gse))
+exprs(gse) <- log2(exprs(gse))
+boxplot(exprs(gse), outline = FALSE)
+View(p)
+library(dplyr)
+
+table(sam$source_name_ch1)
+max(ex)
+View(ex)
+
+gse@annotation
+df <-  read.table(
+    "data/GSE211669/GSE211669_TMM.txt",
+    sep = "\t", header = TRUE,
+    comment.char = "!"
+) 
+
+sample_info <- pData(gse)
+
+sample_info <- dplyr::select(
+    sample_info,
+    source_name_ch1,
+    characteristics_ch1
+)
+
+sample_info <- dplyr::rename(
+    sample_info,
+    group = source_name_ch1,
+    patient = characteristics_ch1
+)
+
+View(sample_info)
+
+library(readr)
+
+library(ggrepel)
+
+library(pheatmap)
+## argument use="c" stops an error if there are any missing data points
+
+cor_matrix <- cor(
+    exprs(gse),
+    use = "c"
+)
+
+pheatmap(cor_matrix)
+
+library(ggplot2)
+suppressPackageStartupMessages()
+library(ggrepel)
+## MAKE SURE TO TRANSPOSE THE EXPRESSION MATRIX
+
+pca <- prcomp(t(exprs(gse)))
+
+## Join the PCs to the sample information
+data <- cbind(
+    sample_info,
+    pca$x
+)
+
+ggplot(
+    data,
+    aes(
+        x = PC1,
+        y = PC2,
+        col = group,
+        label = paste("Patient", patient)
+    )
+) +
+    geom_point() +
+    geom_text_repel()
+
+
+
+View(ex)
+
+library(limma)
+design <- model.matrix(
+    ~0 + sample_info$group
+)
+
+colnames(design) <- c("Normal", "Tumour")
+cutoff <- median(exprs(gse))
+is_expressed <- exprs(gse) > cutoff
+keep <- rowSums(is_expressed) > 2
+table(keep)
+gse <- gse[keep, ]
+fit <- lmFit(exprs(gse), design)
+head(fit$coefficients)
+
+contrasts <- makeContrasts(
+    Tumour - Normal,
+    levels = design
+)
+
+fit2 <- contrasts.fit(fit, contrasts)
+fit2 <- eBayes(fit2)
+topTable(fit2)
+topTable(fit2, coef=1)
+decideTests(fit2)
+table(decideTests(fit2))
+aw <- arrayWeights(exprs(gse), design)
+
+fit <- lmFit(exprs(gse), design,
+             weights = aw)
+contrasts <- makeContrasts(Tumour - Normal, levels=design)
+fit2 <- contrasts.fit(fit, contrasts)
+fit2 <- eBayes(fit2)
+
+anno <- fData(gse)
+View(anno)
+
+annos <- dplyr::select(
+    anno,
+    "Representative Public ID",
+    "Gene Symbol",
+    ENTREZ_GENE_ID,
+    GB_ACC,
+    "Representative Public ID"
+)
+
+View(fit2)
+
+fit2$genes <- anno
+topTable(fit2)
+full_results <- topTable(
+    fit2,
+    number = Inf
+)
+full_results <- tibble::rownames_to_column(
+    full_results,
+    # "GB_ACC"
+)
+View(full_results)
+library(ggplot2)
+ggplot(full_results,aes(x = logFC, y=B)) + geom_point()
+
+p_cutoff <- 0.05
+fc_cutoff <- 1
+
+full_results %>%  # nolint
+  mutate(Significant = adj.P.Val < p_cutoff, abs(logFC) > fc_cutoff) %>% 
+  ggplot(aes(x = logFC, y = B, col=Significant)) + geom_point()
+
+
+library(ggrepel)
+p_cutoff <- 0.05
+fc_cutoff <- 1
+topN <- 40
+
+full_results %>% 
+  mutate(Significant = adj.P.Val < p_cutoff, abs(logFC) > fc_cutoff ) %>% 
+  mutate(Rank = 1: n(), Label = ifelse(Rank < topN, GB_ACC,"")) %>% 
+  ggplot(aes(x = logFC, y = B, col=Significant,label=Label)) + geom_point() + geom_text_repel(col="black")
+
+class(full_results)
+
+filter(full_results, Gene.Symbol == "Bcl2a1")
+
+write.table(
+    full_results,
+    file = "results.csv",
+    quote = FALSE,
+    sep = "\t"
+)
+
+write.table(
+    anno,
+    file = "anno.csv",
+    quote = FALSE,
+    sep = "\t"
+)
--- data/GSE211669/test.R
+++ data/GSE211669/test.R
@@ -1,18 +1,33 @@
-data <- read.table(
+df1 <- read.table(
     "data/GSE211669/GSE211669_TMM.txt",
     header = TRUE,
     sep = "\t"
 )
 
-library(GEOquery)
-my_id <- "GSE211669"
-gse_la <- getGEO(my_id)
-gse_la2 <- getGEO("GSE209964")
+df3 <- read.table(
+    "data/GSE209964_raw.txt",
+    header = TRUE,
+    sep = "\t"
+)
+library("tibble")
+df1 <- as_tibble(df1)
+df3 <- as_tibble(df3)
 
-gse_la[[2]]
+library(GEOquery)
+
+gse_la <- getGEO(
+    GSE211669,
+    destdir = "data/"
+)
+gse_la2 <- getGEO(
+    "GSE209964",
+    destdir = "data/"
+)
+
 pdata1 <- pData(gse_la[[1]])
 pdata2 <- pData(gse_la[[2]])
 pdata3 <- pData(gse_la2[[1]])
+
 pdata1
 pdata2
 pdata3
@@ -27,4 +42,36 @@ pdata2_1 <- filter(
     pdata2["tissue:ch1"] != "Relapse Tumor"
 )
 
+
+table(pdata1["tissue:ch1"])
 table(pdata2["tissue:ch1"])
+table(pdata3["tissue:ch1"])
+
+temp <- seq(
+    from = 8,
+    by = 9,
+    length.out = nrow(df3)
+)
+df4 <- df3 %>% mutate(
+    df3,
+    GeneID = unlist(strsplit(
+        GeneID,
+        split = "\\|"
+    ))[temp]
+)
+
+table(df4["GeneID"])
+
+unlist(strsplit(
+    "sdfsdf|sdfsdf|sdf",
+    split = "\\|"
+))[-3: -2]
+
+df3["GeneID"]
+
+strsplit(
+    df3["GeneID"],
+    split = "\\|"
+)[[1]][-3: -2]
+
+table(df3["GeneID_new"])
